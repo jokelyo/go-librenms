bac24dc4848355ac277ad56c559ee5691f3522ed
jokelyo <joshyost@gmail.com>
2025-06-02 01:15:47 -0500

    add alert rule field

--- alertrule.go
+++ alertrule.go
@@ -12,40 +12,41 @@ const (
 type (
 	// AlertRule represents an alert rule in LibreNMS.
 	AlertRule struct {
-		ID        int     `json:"id"`
-		Builder   string  `json:"builder"`
-		Devices   []int   `json:"devices"`
-		Extra     *string `json:"extra,omitempty"`
-		Groups    []int   `json:"groups"`
-		InvertMap Bool    `json:"invert_map"`
-		Locations []int   `json:"locations"`
-		Name      string  `json:"name"`
-		Notes     *string `json:"notes,omitempty"`
-		Proc      *string `json:"proc,omitempty"`
-		Query     string  `json:"query"`
-		Rule      string  `json:"rule"`
-		Severity  string  `json:"severity"`
+		ID           int     `json:"id"`
+		Builder      string  `json:"builder"`
+		Devices      []int   `json:"devices"`
+		Disabled     Bool    `json:"disabled"`
+		Extra        *string `json:"extra"`
+		Groups       []int   `json:"groups"`
+		InvertMap    Bool    `json:"invert_map"`
+		Locations    []int   `json:"locations"`
+		Name         string  `json:"name"`
+		Notes        *string `json:"notes"`
+		ProcedureURL *string `json:"proc"`
+		Query        string  `json:"query"`
+		Rule         string  `json:"rule"`
+		Severity     string  `json:"severity"`
 	}
 
 	// AlertRuleCreateRequest is the request structure for creating an alert rule.
 	//
 	// Groups and Locations can be empty, but Devices requires a -1 entry for 'all devices'.
 	AlertRuleCreateRequest struct {
-		Builder   string `json:"builder"`
-		Count     int    `json:"count,omitempty"`
-		Delay     string `json:"delay,omitempty"`
-		Devices   []int  `json:"devices"`
-		Disabled  Bool   `json:"disabled,omitempty"`
-		Groups    []int  `json:"groups"`
-		Interval  string `json:"interval,omitempty"`
-		Locations []int  `json:"locations"`
-		Mute      bool   `json:"mute,omitempty"`
-		Name      string `json:"name"`
-		Notes     string `json:"notes,omitempty"`
-		Proc      string `json:"proc,omitempty"`
-		Query     string `json:"query,omitempty"`
-		Rule      string `json:"rule,omitempty"`
-		Severity  string `json:"severity"`
+		Builder      string `json:"builder"`
+		Count        int    `json:"count,omitempty"`
+		Delay        string `json:"delay,omitempty"`
+		Devices      []int  `json:"devices"`
+		Disabled     Bool   `json:"disabled,omitempty"`
+		Groups       []int  `json:"groups"`
+		Interval     string `json:"interval,omitempty"`
+		Locations    []int  `json:"locations"`
+		Mute         bool   `json:"mute,omitempty"`
+		Name         string `json:"name"`
+		Notes        string `json:"notes,omitempty"`
+		ProcedureURL string `json:"proc,omitempty"`
+		Query        string `json:"query,omitempty"`
+		Rule         string `json:"rule,omitempty"`
+		Severity     string `json:"severity"`
 	}
 
 	// AlertRuleUpdateRequest is the request structure for updating an alert rule.
